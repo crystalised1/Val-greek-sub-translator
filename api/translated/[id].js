300ce0fbeacbdbb122edbb20f0f27c0b60f594e3
crystalised1 <kid_creme21@yahoo.gr>
2025-07-23 12:23:59 +0300

    Fix API handler and switch to ES modules

--- api/translated/[id].js
+++ api/translated/[id].js
@@ -1,15 +1,44 @@
-const { translatedCache } = require('../utils');
+import { fetchSubseekerSubs, downloadSrt, translateSrtContent, subsCache, translatedCache } from '../../../utils.js';
 
-module.exports = (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  const id = req.query.id.replace('.srt', '');
-  const srt = translatedCache.get(id);
+  const { id } = req.query;
 
-  if (srt) {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.send(srt);
-  } else {
-    res.status(404).send('Subtitle not found');
+  if (!id) {
+    return res.status(400).json({ error: 'Missing IMDb ID' });
   }
-};
+
+  try {
+    // Check cache first
+    const cached = translatedCache.get(id);
+    if (cached) {
+      return res.status(200).send(cached);
+    }
+
+    // Get subtitle link
+    const subUrl = await fetchSubseekerSubs(id, 'english');
+    if (!subUrl) {
+      return res.status(404).json({ error: 'No subtitles found' });
+    }
+
+    // Download .srt content
+    const srtContent = await downloadSrt(subUrl);
+    if (!srtContent) {
+      return res.status(500).json({ error: 'Failed to download subtitle file' });
+    }
+
+    // Translate content
+    const translated = await translateSrtContent(srtContent);
+
+    // Cache it
+    translatedCache.set(id, translated);
+
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    return res.status(200).send(translated);
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Server error' });
+  }
+}
